757cb294f5fe593ecbe10b7043d26965ca0a4669
luis <lycruzc@yahoo.com>
2025-04-23 18:19:12 -0500

    Add project files.

---
+++ BlazorAI_MCP/BlazorAI_MCP.Web/Services/GitHubMCPService.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.AI;
+using ModelContextProtocol.Client;
+using ModelContextProtocol.Protocol.Transport;
+
+namespace BlazorAI_MCP.Web.Services
+{
+    public class GitHubMCPService
+    {
+        private readonly string[] _toolsToUse = { "get_issue", "search_users" };
+
+        private readonly Task<IMcpClient> _clientTask = McpClientFactory.CreateAsync(
+            new StdioClientTransport(new()
+                {
+                    Command = "npx",
+                    Arguments = ["-y", "@modelcontextprotocol/server-github"],
+                    Name = "GitHub",
+                })
+            );
+        public async Task<IEnumerable<AIFunction>> GetToolsAsync()
+        {
+            var client = await _clientTask;
+            var allTools = await client.ListToolsAsync();
+            return allTools.Where(tool => _toolsToUse.Contains(tool.Name));
+        }
+    }
+
+}
